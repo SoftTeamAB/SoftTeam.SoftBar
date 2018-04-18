2f46253af4f942740f8d6edb6446fc211e4c40ab
Per Hultqvist <per@softteam.se>
2018-04-18 09:01:56 +0200

    Fixed bug removing clipboard images

--- SoftTeam.SoftBar.Core/Clipboard/ClipboardManager.cs
+++ SoftTeam.SoftBar.Core/Clipboard/ClipboardManager.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms;
 using System.Security.Cryptography;
 using System.Text;
 using SoftTeam.SoftBar.Core.SoftBar.Builders;
+using System.Drawing.Imaging;
 
 namespace SoftTeam.SoftBar.Core.ClipboardList
 {
@@ -84,7 +85,7 @@ namespace SoftTeam.SoftBar.Core.ClipboardList
             Image image = Clipboard.GetImage();
             string hash = CalculateHashCode(image);
             // Don't add the image if it is already in the list
-            if (!ContainsHash(hash) && hash != _dontAddHash)
+            if (image != null && !ContainsHash(hash) && hash != _dontAddHash)
             {
                 ClipboardItemImage imageItem = new ClipboardItemImage(image, hash);
                 SetAsCurrentlyInClipboard(imageItem);
@@ -146,10 +147,46 @@ namespace SoftTeam.SoftBar.Core.ClipboardList
             }
             else if (item is ClipboardItemImage)
             {
-                if (Clipboard.ContainsImage() && Clipboard.GetImage() == ((ClipboardItemImage)item).Image)
+                if (Clipboard.ContainsImage() && CompareImages(Clipboard.GetImage(), ((ClipboardItemImage)item).Image))
                     return true;
             }
             return false;
+        }
+
+        private bool CompareImages(Image image1, Image image2)
+        {
+            Bitmap bmp1 = new Bitmap(image1);
+            Bitmap bmp2 = new Bitmap(image2);
+
+            bool equals = true;
+            Rectangle rect = new Rectangle(0, 0, bmp1.Width, bmp1.Height);
+            BitmapData bmpData1 = bmp1.LockBits(rect, ImageLockMode.ReadOnly, bmp1.PixelFormat);
+            BitmapData bmpData2 = bmp2.LockBits(rect, ImageLockMode.ReadOnly, bmp2.PixelFormat);
+            unsafe
+            {
+                byte* ptr1 = (byte*)bmpData1.Scan0.ToPointer();
+                byte* ptr2 = (byte*)bmpData2.Scan0.ToPointer();
+                int width = rect.Width * 3; // for 24bpp pixel data
+                for (int y = 0; equals && y < rect.Height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        if (*ptr1 != *ptr2)
+                        {
+                            equals = false;
+                            break;
+                        }
+                        ptr1++;
+                        ptr2++;
+                    }
+                    ptr1 += bmpData1.Stride - width;
+                    ptr2 += bmpData2.Stride - width;
+                }
+            }
+            bmp1.UnlockBits(bmpData1);
+            bmp2.UnlockBits(bmpData2);
+
+            return equals;
         }
         #endregion
 
